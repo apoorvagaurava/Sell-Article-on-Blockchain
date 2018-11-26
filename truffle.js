0c7a6d9eea90bec2a8ff307e62135346d8e41414
apoorvagaurava <apoorva.gaurava@gmail.com>
2018-11-26 11:03:02 +0000

    Adding frontend files to Github pages

--- truffle.js
+++ truffle.js
@@ -2,17 +2,11 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 //require('./.env');
 module.exports = {
    networks: {
-     development: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*", // Match any network id
-            gas: 4700000
-        },
-     ropsten: {
-      provider: () =>new HDWalletProvider("Paste Your Mnemonic here", "Infura Node Endpoint for Ropsten"),
-     gas: 4500000,
-     gasPrice: 25000000000,
-     network_id: 3
-   }
+        ropsten: {
+      provider: () => new HDWalletProvider("strike leg sausage teach public furnace grape brave crowd help check border","https://ropsten.infura.io/v3/b315d6c071bf47c1af70b4910897056c"),
+      gas: 4500000,
+      gasPrice: 25000000000,
+      network_id: 3
+    }
    }
 }
