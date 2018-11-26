56546d4ca71bf36de84e67345d515a053364a3c7
apoorvagaurava <apoorva.gaurava@gmail.com>
2018-11-26 10:01:51 +0000

    First Commit

--- truffle.js
+++ truffle.js
@@ -1,17 +1,18 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+//require('./.env');
 module.exports = {
-     // See <http://truffleframework.com/docs/advanced/configuration>
-     // to customize your Truffle configuration!
-     networks: {
-          ganache: {
-               host: "localhost",
-               port: 7545,
-               network_id: "*" // Match any network id
-          },
-          chainskills: {
+   networks: {
+     development: {
             host: "localhost",
             port: 8545,
-            network_id: "4224",
+            network_id: "*", // Match any network id
             gas: 4700000
-          }
-     }
-};
+        },
+     ropsten: {
+      provider: () =>new HDWalletProvider("Paste Your Mnemonic here", "Infura Node Endpoint for Ropsten"),
+     gas: 4500000,
+     gasPrice: 25000000000,
+     network_id: 3
+   }
+   }
+}
